c3012e194d80ab64bbf3d9b7c6e9f3eb008b757d
QuentinPerret <92548578+QuentinPerret@users.noreply.github.com>
2022-11-29 16:39:38 +0100

    add first step regression

--- regression.r
+++ regression.r
@@ -1,2 +1,12 @@
 load("donneesProjet2A.RData")
 attach(donneesProjet)
+
+#step ascendant
+res <- lm(Pct.BF ~ 1)
+step(res, ~ Age + Weight + Height + Neck + Chest + Abdomen + Hip + Thigh + Knee + Ankle + Bicep + Forearm + Wrist)
+
+lm(formula = Pct.BF ~ Abdomen + Weight + Wrist + Bicep + Age + Thigh)
+
+#step descendant
+res <- lm(Pct.BF ~ Age + Weight + Height + Neck + Chest + Abdomen + Hip + Thigh + Knee + Ankle + Bicep + Forearm + Wrist)
+step(res)
